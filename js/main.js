d3aebe7fb8d364a663e2aa5d9272209d0cf99dfc
hsdk.bd <berserkd.dhsk@gmail.com>
2015-10-24 20:12:51 -0500

    begin

---
+++ js/main.js
@@ -0,0 +1,12 @@
+Parse.initialize("APPLICATION_ID", "JAVASCRIPT_KEY");
+
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}, {
+    success: function(object) {
+        $(".success").show();
+    },
+    error: function(model, error) {
+        $(".error").show();
+    }
+});
