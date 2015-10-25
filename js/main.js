a188fce81be5ed523d55650c3f54b15e65e90650
hsdk.bd <berserkd.dhsk@gmail.com>
2015-10-24 20:22:21 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-Parse.initialize("APPLICATION_ID", "JAVASCRIPT_KEY");
+//Parse.initialize("APPLICATION_ID", "JAVASCRIPT_KEY");
+Parse.initialize("tm4OiR7E8zVPm6IJvlAclFxfUdWct3QyidSmveDQ", "4dMCCtPUjmpacMw0JCKqhrucl9lyG4FngHkUdaCB");
 
 var TestObject = Parse.Object.extend("TestObject");
 var testObject = new TestObject();
