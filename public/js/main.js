cc72a7a95f76d5ef459bf5fb98836526e03c43aa
hsdk.bd <berserkd.dhsk@gmail.com>
2015-11-01 15:51:08 -0600

    update

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,10 @@
 Parse.initialize("tm4OiR7E8zVPm6IJvlAclFxfUdWct3QyidSmveDQ", "4dMCCtPUjmpacMw0JCKqhrucl9lyG4FngHkUdaCB");
 
 var app = angular.module('Improveise', []);
+//.filter('to_trusted', ['$sce', function($sce){
+//    return function(text) {
+//        return $sce.trustAsHtml(text);
+//    }]);
 
 var DialogueObject = Parse.Object.extend("Dialogue");
 var dialogueObject = new DialogueObject();
@@ -19,52 +23,279 @@ var queryObject = new Parse.Query(DialogueObject);
 ///////////////////////////////////////////////////////////////////////////////////
 
 var socket = io(); //initialise socket.io
-var sendMsg = function() {
-    //alert("asdf");
-    socket.emit('chat message', {
-        username: $('#user_id').val(),
-        msg: $('#user_msg').val(),
-        votes: 0
-    });
-    $('#user_msg').val('');
-    return false;
-};
 
 ///////////////////////////////////////////////////////////////////////////////////
 var threshold_good = 3;
 
 app.controller("MainCtrl", function($scope, $interval) {
-    $scope.messages = [ { username: "undead", msg: "Yo", votes: 0 } ];
+    //$scope.messages = { 1: { username: "undead", userid: "", msg: "Yo", votes: 0 } };
+    $scope.messages = [ { username: "undead", userId: "", msg: "Yo", votes: 0, votesTot: 0 } ];
     $scope.acceptedMsgs = [ "This is a good day to die" ];
+    $scope.username = "";
+    $scope.userId = "";
+    $scope.numStabs = 0;
+    $scope.curScore = 0;
+    $scope.curJudgePoints = 0;
+    $scope.systemId = "__system__";
+    $scope.roundTimeMax = 300; //500;
+    $scope.roundTime = $scope.roundTimeMax;
+    $scope.phaseNo = 0; //0: act, 1: judgement, 2: ended
 
-    socket.on('chat message', function(msgObj){
-        //$('#messages').append($('<li>').text(msg));
-        //alert("received");
-        msgObj.idx = $scope.messages.length;
-        $scope.messages.push(msgObj);
+    $scope.timeMaxForReply = 150;
+    $scope.timeLeftForAction = $scope.timeMaxForReply;
+    $scope.replyingTo = {};
+
+    $scope.numStabsThreshold = 3;
+    $scope.gameover = false;
+    $scope.roundfinished = false;
+
+    socket.on('join round', function(userid){
+        $scope.userId = userid;
+        $scope.phaseNo = 0; //start phase0: act
         $scope.$apply();
     });
+    socket.on('new participant', function(username){
+        $scope.messages.push({
+            userId: $scope.systemId,
+            msg: "< A New sailor has risen from the depths: " + username + " >"
+        });
+        $scope.$apply();
+    });
+    socket.on('user died', function(username){
+        $scope.messages.push({
+            userId: $scope.systemId,
+            msg: "< Sailor: "+ username + " has been killed by the Kraken >"
+        });
+        $scope.$apply();
+    });
+    socket.on('chat message', function(msgObj){
+        $scope.messages.push(msgObj);
+        $scope.$apply();
+        //scroll to bottom
+        var objDiv = document.getElementById("messages");
+        objDiv.scrollTop = 1000000; //objDiv.scrollHeight;
+    });
 
-    $scope.voteUp = function( msgObj ) {
-        msgObj.votes = msgObj.votes+1;
-        if ( msgObj.votes >= threshold_good )
+    $scope.joinRound = function() {
+        if (  $scope.username == "" )
+        {
+            alert("Enter a stage name!");
+            return;
+        }
+        socket.emit('join round', $scope.username);
+    };
+    //send message
+    $scope.sendMsg = function() {
+        //alert("asdf");
+        var msgText = $('#user_msg').val();
+        socket.emit('chat message', {
+            username: $scope.username,
+            userId: $scope.userId,
+            //msg: $scope.replyingTo.username === undefined ? msgText : +"<b>"+msgText+"</b>" + " (to: "+$scope.replyingTo.username+") ",
+            msg: $scope.replyingTo.username === undefined ? msgText : +msgText + " (to: "+$scope.replyingTo.username+") ",
+            votes: 0, //overall + / -
+            votesTot: [],
+            replyingToIdx: $scope.replyingTo == "" ? -1 : $scope.replyingTo.msgIdx
+        });
+        $('#user_msg').val('');
+        $scope.curScore += 10;
+        $scope.resetTimer();
+        if ( $scope.replyingTo.username != "" )
+            $scope.curScore += 50;
+        $scope.replyingTo = {};
+        return false;
+    };
+
+    //[ VOTING ]
+    $scope.voteUp = function( angularMsgObj ) {
+        console.log("voteUp");
+        //angularMsgObj.votes = angularMsgObj.votes+1;
+        //$scope.saveDialogueIfPossible( angularMsgObj );
+        socket.emit('upVote', {
+            judgerUserId: $scope.userId,
+            judgedMsg: angularMsgObj
+        });
+        $scope.curJudgePoints += 50;
+    };
+    $scope.voteDown = function( angularMsgObj ) {
+        //angularMsgObj.votes = angularMsgObj.votes-1;
+        socket.emit('downVote', {
+            judgerUserId: $scope.userId,
+            judgedMsg: angularMsgObj
+        });
+        $scope.curJudgePoints += 50;
+    };
+    socket.on('upVote', function( rel_info ){
+        var msg = $scope.getMsgWithIdx( rel_info.judgedMsg.msgIdx);
+        console.log(msg);
+        console.log("my id:" + $scope.userId);
+        msg.votes++;
+        $scope.pushVote( msg, {
+            voterId: rel_info.judgerUserId,
+            voted: 1
+        });
+        var mine = $scope.getMyVote( msg );
+        console.log(mine);
+        if ( msg.userId == $scope.userId )
+        {
+            $scope.curScore += 10;
+        }
+        $scope.$apply();
+    });
+    socket.on('downVote', function(rel_info){
+        var msg = $scope.getMsgWithIdx( rel_info.judgedMsg.msgIdx);
+        msg.votes--;
+        $scope.pushVote( msg, {
+            voterId: rel_info.judgerUserId,
+            voted: -1
+        });
+        if ( msg.userId == $scope.userId )
+        {
+            alert("You have been stabbed!");
+            $scope.numStabs += 1;
+            $scope.checkGameover();
+        }
+        $scope.$apply();
+    });
+    //make sure 1 user can only make 1 vote for each message
+    $scope.pushVote = function( msg, voteObj )
+    {
+        if ( msg.votesTot )
+        {
+            var found = false;
+            msg.votesTot.forEach(function( vote ) {
+                if ( vote.voterId == voteObj.voterId )
+                {
+                    vote.voted = voteObj.voted;
+                    found = true;
+                }
+            });
+            if ( !found )
+                msg.votesTot.push(voteObj);
+        }
+    };
+    //get user vote for a message
+    $scope.getMyVote = function(msgObj)
+    {
+        var voted = 0;
+        if ( msgObj.votesTot )
+        {
+            msgObj.votesTot.forEach(function( vote ) {
+                if ( vote.voterId == $scope.userId )
+                {
+                    voted = vote.voted;
+                }
+            });
+        }
+        return voted;
+    };
+    //[REPLYING]
+    $scope.replyTarget = function( msgObj )
+    {
+        $scope.replyingTo = {};
+        angular.copy(msgObj, $scope.replyingTo);
+        $scope.$apply();
+    };
+
+    //[ HELPERS ]
+    //get message with idx
+    $scope.getMsgWithIdx = function( msgIdx )
+    {
+        for ( var i = 0; i < $scope.messages.length; ++i )
+        {
+            if ( $scope.messages[i].msgIdx == msgIdx )
+                return $scope.messages[i];
+        }
+    };
+    $scope.decreaseTime = function()
+    {
+        if ( $scope.userId == "" || $scope.phaseNo >= 2 || $scope.gameover ) {
+            //console.log("asdf");
+            return;
+        }
+        //stage: acting
+        if ( $scope.phaseNo == 0 )
+        {
+            $scope.timeLeftForAction = ($scope.timeLeftForAction-0.1).toFixed(2);
+            if ( $scope.timeLeftForAction <= 0 )
+            {
+                $scope.numStabs += 1;
+                $scope.resetTimer();
+                $scope.checkGameover();
+            }
+        }
+        //time up
+        if ( $scope.roundTime <= 0)
+        {
+            ++$scope.phaseNo;
+            //stage: start judgement time
+            if ( $scope.phaseNo == 1 )
+                $scope.roundTime = 60;
+        }
+        //stage: finished
+        if ( $scope.phaseNo == 2 )
+        {
+            $scope.messages.push({
+                userId: $scope.systemId,
+                msg: "< Round Fin! Sailor ("+ $scope.username + ") lands a final blow to the Kraken, sinking it to the depths of the sea >"
+            });
+            $scope.roundfinished = true;
+            $scope.reduceToOnlyGoodDialogue();
+        }
+        //console.log("cont: "+$scope.roundTime);
+        $scope.roundTime = Math.max(0, ($scope.roundTime-0.1)).toFixed(2);
+        //$scope.$apply();
+    };
+    $scope.resetTimer = function()
+    {
+        $scope.timeLeftForAction = $scope.timeMaxForReply;
+    };
+    var timer = $interval( $scope.decreaseTime, 100 );
+    $scope.checkGameover = function()
+    {
+        if ( !$scope.gameover && $scope.numStabs >= $scope.numStabsThreshold )
+        {
+            socket.emit('user died', {
+                userId: $scope.userId,
+                username: $scope.username
+            });
+            $scope.gameover = true;
+        }
+    };
+    $scope.getActualJudgementPoints = function()
+    {
+        var userVote = 0;
+        $scope.messages.forEach(function(msgObj) {
+            var userVote = $scope.getMyVote( msgObj );
+            var majorityVote = 0;
+            if ( userVote != 0 )
+            {
+                msgObj.votesTot.forEach(function(msg) {
+
+                });
+            }
+        });
+    };
+    //reduce to only good dialogue
+    $scope.reduceToOnlyGoodDialogue = function()
+    {
+        $scope.messages.forEach(function(msgObj) {
+            if ( msgObj.votes > 0 && msgObj.username != $scope.systemId )
+                $scope.acceptedMsgs.push(msgObj);
+        });
+    };
+    //save dialogue in db
+    $scope.saveDialogueIfPossible = function( angularMsgObj )
+    {
+        if ( angularMsgObj.votes >= threshold_good )
         {
             console.log("saving...! ");
-            //console.log(msg);
-            var msgObjSend ={
-                username: msgObj.username,
-                msg: msgObj.msg,
-                votes: msgObj.votes
-            };
+            var msgObjSend = createMsgObject( angularMsgObj );
             console.log(msgObjSend);
-            //$scope.acceptedMsgs.push( msg );
-            dialogueObject.save( msgObjSend,
-                //{ foo: "123123" },
-                {
+            dialogueObject.save( msgObjSend, {
                 success: function(object) {
                     console.log(object);
                     $scope.queryAllDialogue();
-                    //pull all
                 },
                 error: function(model, error) {
                     console.log(error);
@@ -72,7 +303,6 @@ app.controller("MainCtrl", function($scope, $interval) {
             })
         }
     };
-
     $scope.queryAllDialogue = function() {
         console.log("querying....!");
         $scope.acceptedMsgs = [ "This is a good day to die" ];
@@ -91,3 +321,13 @@ app.controller("MainCtrl", function($scope, $interval) {
         });
     }
 });
+
+var createMsgObject = function( angularMsgObj )
+{
+    return {
+        username: angularMsgObj.username,
+        userId: angularMsgObj.userId,
+        msg: angularMsgObj.msg,
+        votes: angularMsgObj.votes
+    };
+};
--- server.js
+++ server.js
@@ -5,21 +5,55 @@ app.use(express.static('public'));
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+//var uuid = require('node-uuid');
+
 app.get('/', function(req, res){
     res.sendfile('index.html');
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.broadcast.emit('hi');
+var msgTotal = 100; //just start with 100 for debug purposes
+var alreadyUploaded = false;
 
-    socket.on('disconnect', function(){
+io.on('connection', function(client){ //Note: client usually called "socket"
+    console.log('a user connected');
+    client.broadcast.emit('hi');
+
+    client.on('disconnect', function(){
         console.log('user disconnected');
     });
 
-    socket.on('chat message', function(msg){
+    client.on('join round', function( username ){
+        console.log('user joined round. name:'+username);
+        client.emit( "join round", client.id ); //send only to client
+        io.emit('new participant', username); //send to all
+    });
+
+    client.on('chat message', function(msg){
         console.log('message: ' + msg);
-        io.emit('chat message', msg);
+        ++msgTotal;
+        msg.msgIdx = msgTotal;
+        io.emit('chat message', msg); //send to all
+    });
+
+    client.on('upVote', function( rel_info ){
+        console.log('id:' + rel_info.judgerUserId + " gave upvote to id:"+ rel_info.judgedMsg.userId);
+        io.emit('upVote', rel_info); //send to all
+    });
+
+    client.on('downVote', function(rel_info){
+        console.log('id:' + rel_info.judgerUserId + " gave downvote to id:"+ rel_info.judgedMsg.userId);
+        io.emit('downVote', rel_info); //send to all
+    });
+
+    client.on('user died', function(userObj) {
+        console.log('user: ' + userObj.username + " died (id:"+userObj.userId+")");
+        io.emit('user died', userObj.username); //send to all
+    });
+
+    client.on('upload lines', function() {
+        if ( !alreadyUploaded )
+            client.emit('upload lines', true); //send to only 1 client
+        alreadyUploaded = true;
     });
 });
 
